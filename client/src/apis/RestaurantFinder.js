ce7cd9e746b16c50a42bf22b55568be82df2e51a
Daniel Miller <danielemiller@live.com>
2022-04-01 12:42:12 -0400

    change database configurations

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 /* hosted db vs local db */
-/* postgres://yvtnuqnykqzrhn:127a435c220546ad3d40934a26df469fb3a17cb28d827b1d3f2d1390239aec97@ec2-54-162-207-150.compute-1.amazonaws.com:5432/ddkihbk0qhiips */
+/* (old) postgres://yvtnuqnykqzrhn:127a435c220546ad3d40934a26df469fb3a17cb28d827b1d3f2d1390239aec97@ec2-54-162-207-150.compute-1.amazonaws.com:5432/ddkihbk0qhiips */
 /* http://localhost:4000/api/v1/restaurants */
+/* (new) postgres://fjnifotstgdzuc:54b3ddf42d7e129cc4404f15164772dec0b69a6436b56be969d71788f01ce158@ec2-52-73-155-171.compute-1.amazonaws.com:5432/d8v9b1093d37j6 */
 
 
 export default axios.create({
-    baseURL: 'postgres://yvtnuqnykqzrhn:127a435c220546ad3d40934a26df469fb3a17cb28d827b1d3f2d1390239aec97@ec2-54-162-207-150.compute-1.amazonaws.com:5432/ddkihbk0qhiips',
+    baseURL: 'postgres://fjnifotstgdzuc:54b3ddf42d7e129cc4404f15164772dec0b69a6436b56be969d71788f01ce158@ec2-52-73-155-171.compute-1.amazonaws.com:5432/d8v9b1093d37j6',
 })
